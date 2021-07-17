dadfde4dfd53797ba914954cf33bfa7fa332d564
asramey <andrewramey17@gmail.com>
2021-07-17 14:22:37 -0500

    updated script.js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Get date/time
+var currentTime = moment();
+var saveButton = document.querySelector("#9")
+
+// Get time of day for time blocks
+console.log(currentTime.format("H"));
+
+// Set header to match current date
+var headerDate = currentTime.format("dddd, MMMM Do");
+
+if (headerDate) {
+
+} else {
+
+};
+
+// Set time block colors to match current time of day
+
+// Load from localStorage to page
+var loadStorage = function() {
+
+
+};
+
+// Save functionality
+
+
+// localStorage.setItem("")
+
